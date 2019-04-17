46d9c17323996610a34ae74095ab8c0d4f3add46
rojefferson <rojefferson3@gmail.com>
2019-04-17 19:22:00 -0300

    primeira versao do servidor e dependencias

--- api/api.js
+++ api/api.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'banco.db'
+  });
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -8,3 +14,12 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+//retorna todos os produtos 
+app.get('/produtos', function (req, res) {
+    prod = [];
+    sequelize.query("SELECT * FROM produto").then(myTableRows => {
+        res.send(myTableRows);
+      })
+});
+
