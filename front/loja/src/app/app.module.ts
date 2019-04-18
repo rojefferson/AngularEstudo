74433aad31fc63a4bc5a71247275a3db80d31723
rojefferson <rojefferson3@gmail.com>
2019-04-17 22:28:25 -0300

    adicionando modulo produto

--- front/loja/src/app/app.module.ts
+++ front/loja/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutoComponent } from './produto/produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule,
